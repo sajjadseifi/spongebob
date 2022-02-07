149c7a44635c868af66ab71e9a1e5e8456411d63
SajjadSeifi <s.09334497255@gmail.com>
2022-02-06 20:18:13 -0800

    add 2way for init procces

--- include/process.h
+++ include/process.h
@@ -23,5 +23,29 @@ typedef struct PROCCESS_STRUCT
     list_T* opdev;
 } Proc_T;
 
+/**
+ * @brief 
+ * this function init strickt becuse time & state & opfs & opdev
+ * inited this entry of function  
+ * @param uid  user id created this proccess
+ * @param ppid proccess parent id create  this proccess
+ * @param cmd  command runer tihs proccess proc
+ * @return * Proc_T* 
+ */
+Proc_T* proc_init_stk(size_t uid,size_t pid,size_t ppid,char *cmd,int nice);
+
+/**
+ * @brief 
+ * 
+ * @param uid user id created this proccess
+ * @param pid proccess id
+ * @param ppid proccess parent id create  this proccess
+ * @param time time of start proccess
+ * @param cmd command runer tihs proccess proc
+ * @param nice nice ness peoccess 
+ * @param state type of state proccess 
+ * @return * Proc_T* 
+ */
+Proc_T* proc_init_dyn(size_t uid,size_t pid,size_t ppid,time_t time,char *cmd,int nice,int state);
 
 #endif
