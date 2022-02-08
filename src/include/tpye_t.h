28e199022cf4c15189c31223e3481f6e891066ba
SajjadSeifi <s.09334497255@gmail.com>
2022-02-08 09:13:44 -0800

    add define bit family

--- src/include/tpye_t.h
+++ src/include/tpye_t.h
@@ -1,6 +1,14 @@
 #ifndef TYPET_H
 #define TYPET_H
 #include <stdbool.h>
+#define NOT 0
+#define BIT 1 
+#define NOTC '0'
+#define BITC '1' 
+#define BYTE (8 * BIT)
+#define HALF_BYTE ( BYTE / 2)
+#define BYTE8 (8 * BYTE)
+#define BYTE4 (4 * BYTE)
 
 typedef bool bit_t;
 
